d2ca562f8108a30d8a3c89b7cd625f714924a0b7
Elder Luiz <elderzxvm99@gmail.com>
2023-09-26 20:59:59 -0300

    enable domination for all tanks

--- Amigos infinitos 2.1 (100% Atualizado)/Hooks/charactertweakdata.lua
+++ Amigos infinitos 2.1 (100% Atualizado)/Hooks/charactertweakdata.lua
@@ -5,6 +5,24 @@ Hooks:PostHook(CharacterTweakData, "_init_tank", "_init_tank_sa", function(self,
 		10,
 		15
 	}
+  self.tank_mini.surrender = presets.surrender.always
+  self.tank_mini.priority_shout = nil
+  self.tank_mini.surrender_break_time = {
+  10,
+  15
+}
+self.tank_medic.surrender = presets.surrender.always
+  self.tank_medic.priority_shout = nil
+  self.tank_medic.surrender_break_time = {
+  10,
+  15
+}
+self.tank_hw.surrender = presets.surrender.always
+  self.tank_hw.priority_shout = nil
+  self.tank_hw.surrender_break_time = {
+  10,
+  15
+}
 end)
 
 Hooks:PostHook(CharacterTweakData, "_init_taser", "_init_taser_sa", function(self, presets)
@@ -32,6 +50,37 @@ Hooks:PostHook(CharacterTweakData, "_init_cop", "_init_cop_sa", function(self, p
 		10,
 		15
 	}
+end)
+
+Hooks:PostHook(CharacterTweakData, "_init_heavy_swat", "_init_heavy_swat_sa", function(self, presets)
+  self.heavy_swat.surrender = presets.surrender.always
+  self.heavy_swat.surrender_break_time = {
+  10,
+  15
+}
+end)
+
+Hooks:PostHook(CharacterTweakData, "_init_swat", "_init_swat_sa", function(self, presets)
+  self.swat.surrender = presets.surrender.always
+  self.swat.surrender_break_time = {
+  10,
+  15
+}
+end)
+
+Hooks:PostHook(CharacterTweakData, "_init_fbi", "_init_fbi_sa", function(self, presets)
+  self.fbi.surrender = presets.surrender.always
+  self.fbi.surrender_break_time = {
+  10,
+  15
+}
+end)
+Hooks:PostHook(CharacterTweakData, "_init_fbi_heavy_swat", "_init_fbi_heavy_swat_sa", function(self, presets)
+  self.fbi_heavy_swat.surrender = presets.surrender.always
+  self.fbi_heavy_swat.surrender_break_time = {
+  10,
+  15
+}
 end)
 
 Hooks:PostHook(CharacterTweakData, "_init_gangster", "_init_gangster_sa", function(self, presets)
